bb72cdf6c2a28243c480b0217af94aa5bfa90d3e
e.kushida <e.kushida@st-ventures.jp>
2017-06-22 11:38:43 +0900

    Update Chandel ID

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ios-line-login-demo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+pod 'LineSDK', '~> 4.0.2'  
+
+  # Pods for ios-line-login-demo
+
+  target 'ios-line-login-demoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- ios-line-login-demo/ViewController.swift
+++ ios-line-login-demo/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        LineSDKLogin.sharedInstance().delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func didTapLogin(_ sender: UIButton) {
+        LineSDKLogin.sharedInstance().start()
     }
+}
 
+// MARK: - LineSDKLoginDelegate
+extension ViewController: LineSDKLoginDelegate {
 
+    func didLogin(_ login: LineSDKLogin,
+                  credential: LineSDKCredential?,
+                  profile: LineSDKProfile?,
+                  error: Error?) {
+
+        if error != nil {
+            print(error.debugDescription)
+            return
+        }
+
+        if let accessToken = credential?.accessToken {
+            print("accessToken : \(accessToken)")
+        }
+
+        print("display Name : \(profile?.displayName ?? "unwap display name")")
+        print("userID : \(profile?.userID ?? "unwap userID")")
+
+        if let pictureURL = profile?.pictureURL {
+            print("profile Picture URL : \(pictureURL)")
+        }
+    }
 }
 
